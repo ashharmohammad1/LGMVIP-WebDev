d5420db86e05ca9ad6914166b652978a5d37f25d
ashharmohammad1 <42630846+ashharmohammad1@users.noreply.github.com>
2021-09-20 19:44:42 +0530

    Add files via upload

---
+++ task2/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* <h6> Hi this is vishnu || index.js || this is for footer </h6> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
